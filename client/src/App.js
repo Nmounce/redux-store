cbfa89f94944bd05446eda95e1b76695a5edb99f
Nikki Mounce <nikkimounce@gmail.com>
2021-11-29 15:49:45 -0800

    edited script in server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 import { Provider } from 'react-redux';
 import store from './utils/store';
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const: token = localStorage.getItem('id_token')
+    const token = localStorage.getItem('id_token')
     operation.setContext({
-      headers: { authorization: token ? `Bearer ${token}` : '' }
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
     })
   },
   uri: '/graphql',
-});
+})
 
 function App() {
   return (
@@ -51,6 +47,7 @@ function App() {
         </div>
       </Router>
     </ApolloProvider>
+
   );
 }
 
