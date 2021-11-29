88bcc3c82bc08b21a265f9ae94dd1dfd31aed124
Nikki Mounce <nikkimounce@gmail.com>
2021-11-29 15:37:29 -0800

    edited app.js to include redux mw

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers'
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
-render(
-  <Provider store={store}>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
